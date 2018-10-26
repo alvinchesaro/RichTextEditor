726c8be9aabcad38bc7d7e358ee67c1b168749fd
alvinchesaro <ac@alvinchesaro.com>
2018-10-26 21:57:01 +0300

    Add project files.

---
+++ RichTextEditor/EditorWindow.xaml.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.IO;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls.Ribbon;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace RichTextEditor
+{
+    /// <summary>
+    /// Interaction logic for EditorWindow.xaml
+    /// </summary>
+    public partial class EditorWindow : Window
+    {
+        public EditorWindow()
+        {
+            InitializeComponent();
+            cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+
+            UpdateVisualState();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbFontFamily.SelectedItem != null)
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+        }
+
+
+        void UpdateItemCheckedState(ToggleButton button, DependencyProperty formattingProperty, object expectedValue)
+        {
+            object currentValue = rtbEditor.Selection.GetPropertyValue(formattingProperty);
+            button.IsChecked = (currentValue == DependencyProperty.UnsetValue) ? false : currentValue != null && currentValue.Equals(expectedValue);
+        }
+
+        void ApplyPropertyValueToSelectedText(DependencyProperty formattingProperty, object value)
+        {
+            if (value == null)
+                return;
+            rtbEditor.Selection.ApplyPropertyValue(formattingProperty, value);
+        }
+
+        private void UpdateToggleButtonState()
+        {
+            UpdateItemCheckedState(_btnAlignLeft, Paragraph.TextAlignmentProperty, TextAlignment.Left);
+            UpdateItemCheckedState(_btnAlignCenter, Paragraph.TextAlignmentProperty, TextAlignment.Center);
+            UpdateItemCheckedState(_btnAlignRight, Paragraph.TextAlignmentProperty, TextAlignment.Right);
+            UpdateItemCheckedState(_btnAlignJustify, Paragraph.TextAlignmentProperty, TextAlignment.Right);
+        }
+
+        private void UpdateItemCheckedState(RibbonButton btnAlignLeft, DependencyProperty textAlignmentProperty, TextAlignment left)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateSelectionListType()
+        {
+            Paragraph startParagraph = rtbEditor.Selection.Start.Paragraph;
+            Paragraph endParagraph = rtbEditor.Selection.End.Paragraph;
+            if (startParagraph != null && endParagraph != null && (startParagraph.Parent is ListItem) && (endParagraph.Parent is ListItem) && object.ReferenceEquals(((ListItem)startParagraph.Parent).List, ((ListItem)endParagraph.Parent).List))
+            {
+                TextMarkerStyle markerStyle = ((ListItem)startParagraph.Parent).List.MarkerStyle;
+                if (markerStyle == TextMarkerStyle.Disc) //bullets  
+                {
+                    _btnBullets.IsChecked = true;
+                }
+                else if (markerStyle == TextMarkerStyle.Decimal) //number  
+                {
+                    _btnNumbers.IsChecked = true;
+                }
+            }
+            else
+            {
+                _btnBullets.IsChecked = false;
+                _btnNumbers.IsChecked = false;
+            }
+        }
+        private void UpdateVisualState()
+        {
+            UpdateToggleButtonState();
+            UpdateSelectionListType();
+        }
+
+
+        public void selectImg(RichTextBox rc)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg, *.jpeg,*.gif, *.png) | *.jpg; *.jpeg; *.gif; *.png";
+            var result = dlg.ShowDialog();
+            if (result.Value)
+            {
+                Uri uri = new Uri(dlg.FileName, UriKind.Relative);
+                BitmapImage bitmapImg = new BitmapImage(uri);
+                Image image = new Image();
+                image.Stretch = Stretch.Fill;
+                image.Width = 250;
+                image.Height = 200;
+                image.Source = bitmapImg;
+                var tp = rc.CaretPosition.GetInsertionPosition(LogicalDirection.Forward);
+                new InlineUIContainer(image, tp);
+            }
+        }
+        private void addImage_Click(object sender, RoutedEventArgs e)
+        {
+            selectImg(rtbEditor);
+        }
+
+        private void fontcolor(RichTextBox rc)
+        {
+            var colorDialog = new System.Windows.Forms.ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var wpfcolor = Color.FromArgb(colorDialog.Color.A, colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+                TextRange range = new TextRange(rc.Selection.Start, rc.Selection.End);
+                range.ApplyPropertyValue(FlowDocument.ForegroundProperty, new SolidColorBrush(wpfcolor));
+            }
+        }
+
+
+        private void SettingsFontColor_Click(object sender, RoutedEventArgs e)
+        {
+            fontcolor(rtbEditor);
+        }
+
+        private void btnSavetoFolder_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            // set a default file name  
+            savefile.FileName = "unknown.doc";
+            // set filters - this can be done in properties as well  
+            savefile.Filter = "Document files (*.doc)|*.doc";
+            if (savefile.ShowDialog() == true)
+            {
+                TextRange t = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+                FileStream file = new FileStream(savefile.FileName, FileMode.Create);
+                t.Save(file, System.Windows.DataFormats.Rtf);
+                file.Close();
+            }
+
+
+        }
+    }
+}
